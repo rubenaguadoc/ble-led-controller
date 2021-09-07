d78af00b5d8e472c05245ccd9ad9a67e0ea2231c
rubenaguadoc <rubenaguadoc@gmail.com>
2021-09-07 09:21:07 +0200

    Ajustes tras usar un par de días

--- router.js
+++ router.js
@@ -22,18 +22,18 @@ async function sleepService({ event }) {
     if (!connected()) await connect();
     await dim(1, [255, 25, 0], 0.1);
     await run(COMMANDS.ON);
-    await dim(100, [255, 255, 0], 45);
-    await dim(100, [255, 255, 255], 15);
+    await dim(100, [255, 255, 0], 90);
+    await dim(100, [255, 255, 255], 30);
     await disconnect();
   } else if (event === 'alarm_snooze_clicked') {
     if (!connected()) await connect();
     await dim(1, [255, 35, 0], 2);
     await run(COMMANDS.OFF);
+    await new Promise((resolve, reject) => setTimeout(resolve, 500));
     await disconnect();
   } else if (event === 'alarm_alert_dismiss') {
     if (!connected()) await connect();
-    await dim(100, [255, 255, 0], 5);
-    await dim(100, [255, 255, 255], 2);
+    await dim(60, [255, 75, 0], 5);
     await disconnect();
   }
 }
